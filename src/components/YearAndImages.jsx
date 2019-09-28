b48fbe6b0fc744878b18b965ec844293b974da99
mowshe <6mikeski13@gmail.com>
2019-09-28 17:52:12 -0500

    Working on mouse hover

--- src/components/YearAndImages.jsx
+++ src/components/YearAndImages.jsx
@@ -8,12 +8,22 @@ class YearAndImages extends React.Component {
         this.state ={
             visible:false
         }
+    
+        const img1 = this.props.activeProject.image1;
+        console.log(img1 + "AHH");
+        const imgArray =[
+            this.props.activeProject.image1
+        ]
+    
     }
+    
+    
     render(){
         if (this.props.activeProject === this.props.projectName){
             return(
                 <div class="yearAndImages"> 
                     <span id="year" style={yearStyle}>{this.props.year}</span>
+                    <img id="testtt" style={imgStyle}></img>
                     {/* <span id="hoverImage" style={imgStyle}>{GreenUnit}</span> */}
                     {/* <span id="titleNameProjectList">Sponge sponge sponge</span> */}
                     {/* <img id="yearImage" src={GreenUnit}/> */}
@@ -27,9 +37,12 @@ const yearStyle = {
     // SHOULD display on cursor
 }
 
+
 const imgStyle = {
     width:'500px',
     height:'500px',
+
+
 }
 
 export default YearAndImages;
