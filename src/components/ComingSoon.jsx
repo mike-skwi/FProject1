703615238405f0f4af31942e7666db653737eafb
mowshe <6mikeski13@gmail.com>
2019-10-02 12:13:26 -0500

    doing 5k res

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ComingSoon extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+}
+
+CanvasRenderingContext2D(){
+    return(
+        <div id="comingSoonPage">
+            <h1>Coming Soon</h1>
+
+
+        </div>
+    );
+}
+
+export default ComingSoon;
