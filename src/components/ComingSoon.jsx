260f885458f9bd9d3e10dc8de5f329e677c59b0f
mowshe <6mikeski13@gmail.com>
2019-10-08 23:55:17 -0500

    Added text and icons to same div

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -33,17 +33,18 @@ class ComingSoon extends React.Component{
                         <div id="textAndIcons">
                             <p id='comingSoonText'>Coming Soon
                             </p>
-                        </div>
-                        <div id="socialIcons"> 
-                            <a href="http://www.twitter.com/cooolsocial" target="_blank" itemprop="url">
-                                <img value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
-                            </a>
-                            <a href="http://www.instagram.com/cooolsocial" target="_blank" itemprop="url">
-                                <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/co_rgb:db5545,e_colorize:70/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
-                            </a>
+                            <div id="socialIcons"> 
+                                <a href="http://www.twitter.com/cooolsocial" target="_blank" itemprop="url">
+                                    <img value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
+                                </a>
+                                <a href="http://www.instagram.com/cooolsocial" target="_blank" itemprop="url">
+                                    <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/co_rgb:db5545,e_colorize:70/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
+                                </a>
 
-                            <img value='mail' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                                <img value='mail' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                            </div>
                         </div>
+
                     </div>
                 );
             }
@@ -54,15 +55,15 @@ class ComingSoon extends React.Component{
                         <div id="textAndIcons">
                             <p id='comingSoonText'>Coming Soon
                             </p>
-                        </div>
-                        <div id="socialIcons">
-                            <a href="http://www.twitter.com/cooolsocial" target="_blank" itemprop="url">
-                                <img  value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/co_rgb:0079b5,e_colorize:70/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
-                            </a>
-                            <a href="http://www.instagram.com/cooolsocial" target="_blank" itemprop="url">
-                                <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
-                            </a>
-                            <img onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                            <div id="socialIcons">
+                                <a href="http://www.twitter.com/cooolsocial" target="_blank" itemprop="url">
+                                    <img  value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/co_rgb:0079b5,e_colorize:70/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
+                                </a>
+                                <a href="http://www.instagram.com/cooolsocial" target="_blank" itemprop="url">
+                                    <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
+                                </a>
+                                <img onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                            </div>
                         </div>
                     </div>
                 );
@@ -74,25 +75,25 @@ class ComingSoon extends React.Component{
                         <div id="textAndIcons">
                             <p id='comingSoonText'>Coming Soon
                             </p>
-                        </div>
-                        <div id="socialIcons">
-                        <img value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
-                            <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
-                            <img value='mail' onMouseEnter={this.hoverOverImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,co_rgb:2a2f47,e_colorize:100,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
-                                            
-                                            
-                        <Spring
-                        from={{marginTop:-30}}
-                        to={{marginTop:0}}
-                        config={{delay:0, duration:100}}>
-                        {props=>(
-                            <div  style={props}>
-                                <p id='mailText'>info@itscoool.com</p>
+                            <div id="socialIcons">
+                            <img value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
+                                <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
+                                <img value='mail' onMouseEnter={this.hoverOverImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,co_rgb:2a2f47,e_colorize:100,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                                                
+                                                
+                            <Spring
+                            from={{marginTop:-30}}
+                            to={{marginTop:0}}
+                            config={{delay:0, duration:100}}>
+                            {props=>(
+                                <div  style={props}>
+                                    <p id='mailText'>info@itscoool.com</p>
+
+                                </div>
+                            )}
+                        </Spring>
 
                             </div>
-                        )}
-                    </Spring>
-
                         </div>
                     </div>
                 );
@@ -106,15 +107,15 @@ class ComingSoon extends React.Component{
                     <div id="textAndIcons">
                         <p id='comingSoonText'>Coming Soon
                         </p>
-                    </div>
-                    <div id="socialIcons">
-                    <a href="http://www.twitter.com/cooolsocial" target="_blank" itemprop="url">
-                        <img value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
-                    </a>
-                    <a href="http://www.instagram.com/cooolsocial/" target="_blank" itemprop="url">
-                        <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
-                    </a>
-                        <img  onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                        <div id="socialIcons">
+                        <a href="http://www.twitter.com/cooolsocial" target="_blank" itemprop="url">
+                            <img value='twitter' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="twitIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/twitter_PNG15_ixivk3.png"/>
+                        </a>
+                        <a href="http://www.instagram.com/cooolsocial/" target="_blank" itemprop="url">
+                            <img value='insta' onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="instaIcon" src="https://res.cloudinary.com/coool/image/upload/v1570050941/icons/Instagram-icon-WHITE_qtg9uh.png"/>
+                        </a>
+                            <img  onMouseEnter={this.hoverOverImage} onMouseLeave={this.leaveImage} id="mailIcon" src="https://res.cloudinary.com/coool/image/upload/c_crop,h_920,w_1200/v1570050941/icons/584856b4e0bb315b0f7675ac_fqtcpf.png"/>
+                        </div>
                     </div>
                 </div>
             );
